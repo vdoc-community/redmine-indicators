32ec5529406f3b0b3fb028b864b844cbda6c97a0
famaridon <famaridon@users.noreply.github.com>
2018-12-12 13:51:07 +0100

    add multiple status

--- src/app/card/support/support.component.ts
+++ src/app/card/support/support.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit} from '@angular/core';
-import {IssuesService} from '../../services/issues.service';
-import {Observable} from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { IssuesService } from '../../services/issues.service';
+import { Observable } from 'rxjs';
 import { SimpleIndicator } from 'src/app/beans/simple-indicator';
 
 @Component({
@@ -8,16 +8,21 @@ import { SimpleIndicator } from 'src/app/beans/simple-indicator';
   templateUrl: './support.component.html'
 })
 export class SupportComponent implements OnInit {
-
+  public status: 'loading' | 'success' | 'error' = 'loading';
   public count: SimpleIndicator;
 
   constructor(private issuesService: IssuesService) {
   }
 
   ngOnInit() {
-    this.issuesService.findSupportIssues().subscribe((simpleIndicator: SimpleIndicator) => {
-      this.count = simpleIndicator;
-    });
+    this.issuesService.findSupportIssues().subscribe(
+      (simpleIndicator: SimpleIndicator) => {
+        this.count = simpleIndicator;
+        this.status = 'success';
+      },
+      (error: any) => {
+        this.status = 'error';
+      });
   }
 
 }
