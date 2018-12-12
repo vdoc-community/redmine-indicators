7d457d8e1e4f65ab46bdcc14c1a1e89a397fa7c3
omassol <omassol@visiativ.com>
2018-12-12 13:42:26 +0100

    Ids for redmine elements are now in configuration file

--- src/app/services/issues.service.ts
+++ src/app/services/issues.service.ts
@@ -5,6 +5,7 @@ import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { SimpleIndicator } from '../beans/simple-indicator';
 import { RedmineIndicatorsService } from './redmine-indicators.service';
+import { ConfigurationService } from './configuration.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +13,12 @@ import { RedmineIndicatorsService } from './redmine-indicators.service';
 export class IssuesService {
   private httpClient: HttpClient;
   private redmineIndicatorsService: RedmineIndicatorsService;
+  private configurationService: ConfigurationService;
 
-  constructor(httpClient: HttpClient, redmineIndicatorsService: RedmineIndicatorsService) {
+  constructor(httpClient: HttpClient, redmineIndicatorsService: RedmineIndicatorsService, configurationService: ConfigurationService) {
     this.httpClient = httpClient;
     this.redmineIndicatorsService = redmineIndicatorsService;
+    this.configurationService = configurationService;
   }
 
   public findOpenTickets(iteration: Iteration, category: Category): Observable<number> {
@@ -24,13 +27,20 @@ export class IssuesService {
 
   public findSupportIssues(): Observable<SimpleIndicator> {
     return this.httpClient.get<SimpleIndicator>(
-      `${this.redmineIndicatorsService.getBackendUrl()}/v1/issues/project/1032/query/1762/count/`
+      this.buildSupportIssuesUrl(
+        this.redmineIndicatorsService.getBackendUrl(),
+        this.configurationService.getSupportProjectId(),
+        this.configurationService.getRDOpenQueryId())
       , {
         headers: {
           "X-Redmine-API-Key": this.redmineIndicatorsService.getXRedmineApiKey()
         }
       }
     );
+  }
+
+  private buildSupportIssuesUrl(backendUrl: string, projectId: number, queryId: number): string {
+    return `${backendUrl}/v1/issues/project/${projectId}/query/${queryId}/count/`;
   }
 
 }
