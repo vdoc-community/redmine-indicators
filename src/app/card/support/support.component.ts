3c0d2f38e84d508318ad9fc9b7ce5094f6e37ed9
famaridon <famaridon@users.noreply.github.com>
2019-01-04 13:04:11 +0100

    support disconection

--- src/app/card/support/support.component.ts
+++ src/app/card/support/support.component.ts
@@ -1,32 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { IssuesService } from '../../services/issues.service';
-import { Observable, interval, timer } from 'rxjs';
+import { timer, of, Subscription } from 'rxjs';
 import { SimpleIndicator } from 'src/app/beans/simple-indicator';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-support',
   templateUrl: './support.component.html'
 })
-export class SupportComponent implements OnInit {
+export class SupportComponent implements OnInit, OnDestroy {
+
+  private issuesService: IssuesService;
+
   public status: 'loading' | 'success' | 'error' = 'loading';
   public count: SimpleIndicator;
-  private issuesService: IssuesService;
+  private timmer: Subscription;
 
   constructor(issuesService: IssuesService) {
     this.issuesService = issuesService;
   }
 
   ngOnInit() {
-    timer(0, 15000)
-      .pipe(switchMap(_ => this.issuesService.findSupportIssues()))
-      .subscribe((simpleIndicator: SimpleIndicator) => {
+    this.timmer = timer(0, 15000)
+      .pipe(switchMap(_ => this.issuesService.findSupportIssues().pipe(catchError((error: Error) => {
+        return of(error);
+      }))))
+      .subscribe((simpleIndicator: SimpleIndicator | Error) => {
+        if (simpleIndicator instanceof Error) {
+          this.count = null;
+          this.status = 'error';
+        } else {
           this.count = simpleIndicator;
           this.status = 'success';
-        },
-        (error: any) => {
-          this.status = 'error';
         }
+      }
       );
+  }
+
+  ngOnDestroy(): void {
+    this.timmer.unsubscribe();
   }
 }
