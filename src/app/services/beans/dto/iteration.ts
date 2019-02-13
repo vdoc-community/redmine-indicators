c87c2c70c28661ee036125947fbfa51e4824b27e
famaridon <famaridon@users.noreply.github.com>
2019-02-13 18:11:53 +0100

    add plannedDevelopmentCost

--- src/app/services/beans/dto/iteration.ts
+++ src/app/services/beans/dto/iteration.ts
@@ -4,11 +4,13 @@ export class Iteration extends AbstractBean {
   start: Date;
   end: Date;
   number: number;
+  plannedDevelopmentCost: number;
 }
 
 export function parseIteration(json: any): Iteration {
   const iteration = new Iteration(json.id, json.name);
   iteration.number = json.number;
+  iteration.plannedDevelopmentCost = json.plannedDevelopmentCost;
   iteration.start = new Date(json.start);
   iteration.end = new Date(json.end);
   return iteration;
